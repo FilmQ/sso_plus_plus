349336d30eb41b2d4eee6f287d0e0c946611460e
FilmQ <directedtofilm@gmail.com>
2026-09-02 14:00:53 +0700

    prepare main func for future dev

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'SSO Plus Plus',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
       home: Scaffold(
-         
+
       ),
       routes: {
         /* insert route here */
